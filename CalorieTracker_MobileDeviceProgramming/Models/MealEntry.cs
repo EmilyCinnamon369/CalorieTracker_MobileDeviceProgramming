8363efd3fb66deb5ff0cf085b2a4e3378bf80be6
EmilyCinnamon369 <emilycinnamon369@gmail.com>
2026-04-22 01:25:44 -0400

    Created model for meal entries, updated AddFoodPage

---
+++ CalorieTracker_MobileDeviceProgramming/Models/MealEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalorieTracker_MobileDeviceProgramming.Models
+{
+    public class MealEntry
+    {
+        public int Id { get; set; }
+        [Required] public string MealName { get; set; }
+        [Required] public string MealDescription { get; set; }
+        public int Calories { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
